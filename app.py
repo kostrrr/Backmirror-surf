ced046b6b2e5b33a6e9bbf9ba121c3061e0fc168
kostrrr <mkostrrr@gmail.com>
2026-04-27 15:38:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -117,12 +117,12 @@ document.querySelectorAll(".session").forEach(e=>{
 """  
 @app.route("/")
 def main():
-return render_template_string(
-HTML,
-days=DAYS,
-sessions=prepare_sessions(),
-h=time_to_px("22:00")
-)
+    return render_template_string(
+        HTML,
+        days=DAYS,
+        sessions=prepare_sessions(),
+        h=time_to_px("22:00")
+    )
 if name == "main":
 port = int(os.environ.get("PORT", 10000))
 app.run(host="0.0.0.0", port=port)
