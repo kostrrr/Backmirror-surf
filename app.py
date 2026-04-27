f4f456a5bdfc16a426d45bfae6ff4aaefe84b8c6
kostrrr <mkostrrr@gmail.com>
2026-04-27 15:39:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,6 +123,6 @@ def main():
         sessions=prepare_sessions(),
         h=time_to_px("22:00")
     )
-if name == "main":
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
